9cfca61d8afb799684dcd1e2f9319470497873b4
Daniel Ashcroft <dashcroft4@gmail.com>
2020-01-11 22:51:07 +0000

    added close option

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+
+	// Options for the user
+	fmt.Println("Enter a number 1 to 6 to pick an option!")
+	fmt.Println("1 : New deck")
+	fmt.Println("2 : Print")
+	fmt.Println("3 : Shuffle")
+	fmt.Println("4 : Deal")
+	fmt.Println("5 : Save a deck to a file")
+	fmt.Println("6 : get a deck from a file")
+	fmt.Println("7 : close")
+
+	fmt.Print("Enter the number identifying your action :")
 }
